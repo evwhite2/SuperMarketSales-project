5651063258a8ac234a40f5669c1b7e82a9ef2e6c
Ellen White <evwhite2@github.com>
2021-10-12 17:27:22 -0500

    adding dummies folder and python file

---
+++ App/dummies/supermarket_dummies.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import datetime
+
+ct = datetime.datetime.now()
+
+file=pd.read_csv("./supermarket_sales.csv")
+
+# print("\n---------------------------------------------\n", "run timestamp: ", ct, "\n---------------------------------------------")
+
+#renaming colums with spaces
+file.rename(columns = {'Invoice ID':'invoice_ID', 'Customer type':'customer_type', 'Product line':'product_line', 'Unit price':'unit_price', 'Tax 5%':'tax_5%', 'gross margin percentage':'gross_margin_%', 'gross income':'gross_income'}, inplace = True)
+
+#listing all of the types of of products in order to create dummie variables
+print(file.head())
+product=file['product_line']
+print(product.value_counts())
+
+
+#changing product_line names
+file.product_line.replace(to_replace=['Fashion accessories'], value= ["fashion_accessories"], inplace=True)
+file.product_line.replace(to_replace=['Food and beverages'], value= ["food_bev"], inplace=True)
+file.product_line.replace(to_replace=['Electronic accessories'], value= ["electronic_accessories"], inplace=True)
+file.product_line.replace(to_replace=['Sports and travel'], value= ["sports_travel"], inplace=True)
+file.product_line.replace(to_replace=['Home and lifestyle'], value= ["home_lifesyle"], inplace=True)
+file.product_line.replace(to_replace=['Health and beauty'], value= ["health_beauty"], inplace=True)
+
+#product_line dummy variables
+product_series=file.product_line
+                     
+product= pd.get_dummies(product_series, columns=['product_line'])
+
+file['fashion_accessories']=product.fashion_accessories
+file['food_bev']=product.food_bev
+file['electronic_accessories']= product.electronic_accessories
+file['sports_travel']= product.sports_travel
+file['home_lifesyle']= product.home_lifesyle
+file['health_beauty']= product.health_beauty
+
+
+#creating dummmy vaiables for gender
+gender_series=file.Gender
+
+gender_dummies=pd.get_dummies(gender_series, columns=['Gender'])
+
+file['Gender_Male1']= gender_dummies.Male
+
+
+#payment dummy variables
+file.Payment.replace(to_replace=['Credit card'], value= ["credit_card"], inplace=True)
+
+payment_series=file.Payment
+                     
+payment= pd.get_dummies(payment_series, columns=['Payment'])
+
+file['Ewallet']=payment.Ewallet
+file['Cash']=payment.Cash
+file['credit_card']= payment.credit_card
+
+
+
+
+
+#customer_type dummy variables
+customer_series=file.customer_type
+
+members=pd.get_dummies(customer_series, columns=['customer_type'])
+
+file['member_1']= members.Member
+
+
+#city dummy variables
+city_series=file.City
+
+city=pd.get_dummies(city_series, columns=['City'])
+
+file['Yangon']= city.Yangon
+file['Naypyitaw']= city.Naypyitaw
+file['Mandalay']= city.Mandalay
+
+
+
+
+#chaning the layout of the page
+file=file[['invoice_ID', 'Branch', 'City','Yangon', 'Naypyitaw','Mandalay', 'customer_type','member_1', 'Gender','Gender_Male1', 'product_line','fashion_accessories','food_bev', 'electronic_accessories','sports_travel', 'home_lifesyle', 'health_beauty', 'unit_price', 'Quantity', 'tax_5%', 'Total', 'Date','Time', 'Payment','Ewallet', 'Cash', 'credit_card', 'cogs', 'gross_margin_%', 'gross_income','Rating']]
+
+
+
+new_file= file.to_csv("./new_supermarket_dummy_data.csv")
